a018495334343b149aa0e20c00f98633a219f079
haibo <haibo.zhu@hotmail.com>
2018-11-08 13:14:03 +0800

    add db_init.py to work standalone

---
+++ db_init.py
@@ -0,0 +1,12 @@
+#########################################
+# Author         : Haibo Zhu             
+# Email          : haibo.zhu@hotmail.com 
+# created        : 2018-11-05 10:21 
+# Last modified  : 2018-11-05 15:29
+# Filename       : db_init.py
+# Description    :                       
+#########################################
+
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+
